03184705f769ae0792fb10a8560327b02b6102a6
sav-dev <tomasz.sawicki.dev@outlook.com>
2019-12-06 20:04:53 -0800

    Revert "controller palette sound"
    This reverts commit 78146c163653ca732190c6b19aa629558922c828.

--- lib/soundController.asm
+++ lib/soundController.asm
@@ -20,23 +20,23 @@ SongsEnd:
 
 InitializeSound:
   LDA #SOUND_REGION_NTSC
-  STA <sound_param_byte_0
+  STA sound_param_byte_0
   LDA #LOW(song_list)
-  STA <sound_param_word_0
+  STA sound_param_word_0
   LDA #HIGH(song_list)
-  STA <(sound_param_word_0 + $01)
+  STA sound_param_word_0 + $01
   LDA #LOW(sfx_list)
-  STA <sound_param_word_1
+  STA sound_param_word_1
   LDA #HIGH(sfx_list)
-  STA <(sound_param_word_1 + $01)
+  STA sound_param_word_1 + $01
   LDA #LOW(envelopes_list)
-  STA <sound_param_word_2
+  STA sound_param_word_2
   LDA #HIGH(envelopes_list)
-  STA <(sound_param_word_2 + $01)
+  STA sound_param_word_2 + $01
   ;LDA #LOW(dpcm_list)
-  ;STA <sound_param_word_3
+  ;STA sound_param_word_3
   ;LDA #HIGH(dpcm_list)
-  ;STA <(sound_param_word_3 + $01)
+  ;STA sound_param_word_3 + $01
   JSR sound_initialize
 
 ;****************************************************************
@@ -50,7 +50,7 @@ InitializeSound:
 PlaySong:
   ; todo 0006 - keep the track of currently played song
   LDA #song_index_song
-  STA <sound_param_byte_0
+  STA sound_param_byte_0
   JSR play_song
   RTS
   
@@ -75,9 +75,9 @@ StopSong:
 
 SfxShot:
   LDA #sfx_index_sfx_shot
-  STA <sound_param_byte_0
+  STA sound_param_byte_0
   LDA #soundeffect_one
-  STA <sound_param_byte_1
+  STA sound_param_byte_1
   JMP play_sfx
   
 ;****************************************************************
@@ -90,9 +90,9 @@ SfxShot:
 
 SfxExplode:
   LDA #sfx_index_sfx_explode
-  STA <sound_param_byte_0
+  STA sound_param_byte_0
   LDA #soundeffect_one
-  STA <sound_param_byte_1
+  STA sound_param_byte_1
   JMP play_sfx
   
 ;****************************************************************
