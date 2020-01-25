c473619cd46aa88f8cc03335e97b51ea00bbe9d0
sav-dev <tomasz.sawicki.dev@outlook.com>
2020-01-24 22:46:28 -0800

    fix

--- sound/sound.asm
+++ sound/sound.asm
@@ -66,6 +66,7 @@ volume:
   .dw volume8
   .dw volume9
   .dw volume10
+  .dw volume11
 
 arpeggio:
   .dw arpeggio0
@@ -78,6 +79,7 @@ pitch:
   .dw pitch2
   .dw pitch3
   .dw pitch4
+  .dw pitch5
 
 duty:
   .dw duty0
@@ -104,6 +106,8 @@ volume8:
 volume9:
   .db 4,3,2,1,0,ENV_STOP
 volume10:
+  .db 5,5,5,4,4,4,4,4,4,4,4,4,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,0,ENV_STOP
+volume11:
   .db 0,ENV_STOP
 
 arpeggio0:
@@ -122,10 +126,12 @@ pitch2:
 pitch3:
   .db 5,45,75,ENV_STOP
 pitch4:
+  .db 0,1,2,1,0,ENV_LOOP,0
+pitch5:
   .db 0,ENV_STOP
 
 duty0:
-  .db 0,DUTY_ENV_STOP
+  .db 64,DUTY_ENV_STOP
 duty1:
   .db 0,DUTY_ENV_STOP
 
@@ -153,21 +159,20 @@ _song_title_noise_loop:
   .dw _song_title_noise_loop
 
 _song_title_square2_0:
-  .db STV,7,SAR,2,STP,4,SDU,1,SL0,C3,SL8,E3,F3,SL0,C3,SL8,E3,F3
+  .db STV,7,SAR,2,STP,5,SDU,1,SL0,C3,SL8,E3,F3,SL0,C3,SL8,E3,F3
   .db RET
 
 _song_title_noise_0:
-  .db STV,8,SAR,1,STP,4,SDU,1,SL2,1,STV,9,SAR,2,SL1,13,13,STV,8
-  .db SAR,1,SL2,6,STV,9,SAR,2,SL1,13,13,STV,8,SAR,1,SL2,1,STV,9
-  .db SAR,2,SL1,13,13,STV,8,SAR,1,SL2,6,STV,9,SAR,2,SL1,13,13,STV,8
-  .db SAR,1,SL2,1,STV,9,SAR,2,SL1,13,13,STV,8,SAR,1,SL2,6,STV,9
-  .db SAR,2,SL1,13,13,STV,8,SAR,1,SL2,1,STV,9,SAR,2,SL1,13,13,STV,8
-  .db SAR,1,SL2,6,STV,9,SAR,2,SL1,13,13,STV,8,SAR,1,SL2,1,STV,9
-  .db SAR,2,SL1,13,13,STV,8,SAR,1,SL2,6,STV,9,SAR,2,SL1,13,13,STV,8
-  .db SAR,1,SL2,1,STV,9,SAR,2,SL1,13,13,STV,8,SAR,1,SL2,6,STV,9
-  .db SAR,2,SL1,13,13,STV,8,SAR,1,SL2,1,STV,9,SAR,2,SL1,13,13,STV,8
-  .db SAR,1,SL2,6,STV,9,SAR,2,SL1,13,13,STV,8,SAR,1,SL2,1,STV,9
-  .db SAR,2,SL1,13,13,STV,8,SAR,1,SL2,6,STV,9,SAR,2,SL1,13,13
+  .db STV,8,SAR,2,STP,5,SDU,1,SL2,1,STV,9,SL1,13,13,STV,8,SAR,1
+  .db SL2,6,STV,9,SAR,2,SL1,13,13,STV,8,SL2,1,STV,9,SL1,13,13,STV,8
+  .db SAR,1,SL2,6,STV,9,SAR,2,SL1,13,STV,8,14,SL2,1,STV,9,SL1
+  .db 13,13,STV,8,SAR,1,SL2,6,STV,9,SAR,2,SL1,13,13,STV,8,SL2
+  .db 1,STV,9,SL1,13,13,STV,8,SAR,1,SL2,6,STV,9,SAR,2,SL1,13,STV,8
+  .db 14,SL2,1,STV,9,SL1,13,13,STV,8,SAR,1,SL2,6,STV,9,SAR,2
+  .db SL1,13,13,STV,8,SL2,1,STV,9,SL1,13,13,STV,8,SAR,1,SL2,6,STV,9
+  .db SAR,2,SL1,13,STV,8,14,SL2,1,STV,9,SL1,13,13,STV,8,SAR,1
+  .db SL2,6,STV,9,SAR,2,SL1,13,13,STV,8,SL2,1,STV,9,SL1,13,13,STV,8
+  .db SAR,1,SL2,6,SAR,2,SL1,13,14
   .db RET
 
 _song_story:
@@ -287,7 +292,7 @@ _sfx_option_changed_square1:
   .db CAL,low(_sfx_option_changed_square1_0),high(_sfx_option_changed_square1_0)
   .db TRM
 _sfx_option_changed_square1_0:
-  .db STV,1,SAR,2,STP,4,SDU,1,SLL,122,SLH,1,B3,SLL,4,B3
+  .db STV,1,SAR,2,STP,5,SDU,1,SLL,122,SLH,1,B3,SLL,4,B3
   .db RET
 
 _sfx_pause:
@@ -303,7 +308,7 @@ _sfx_pause_square1:
   .db CAL,low(_sfx_pause_square1_0),high(_sfx_pause_square1_0)
   .db TRM
 _sfx_pause_square1_0:
-  .db STV,0,SAR,2,STP,0,SDU,1,SLL,6,D4,C4,SLL,1,STV,2,STP,4
+  .db STV,0,SAR,2,STP,0,SDU,1,SLL,6,D4,C4,SLL,1,STV,2,STP,5
   .db C4
   .db RET
 
@@ -320,7 +325,7 @@ _sfx_keycard_square1:
   .db CAL,low(_sfx_keycard_square1_0),high(_sfx_keycard_square1_0)
   .db TRM
 _sfx_keycard_square1_0:
-  .db STV,1,SAR,2,STP,4,SDU,1,SLL,6,F4,FS4,SLL,4,G4
+  .db STV,1,SAR,2,STP,5,SDU,1,SLL,6,F4,FS4,SLL,4,G4
   .db RET
 
 _sfx_blinker:
@@ -336,7 +341,7 @@ _sfx_blinker_square1:
   .db CAL,low(_sfx_blinker_square1_0),high(_sfx_blinker_square1_0)
   .db TRM
 _sfx_blinker_square1_0:
-  .db SLL,9,STV,3,SAR,0,STP,4,SDU,1,G1
+  .db SLL,9,STV,3,SAR,0,STP,5,SDU,1,G1
   .db RET
 
 _sfx_shot_player:
@@ -432,7 +437,7 @@ _sfx_expl_player_noise:
   .db CAL,low(_sfx_expl_player_noise_0),high(_sfx_expl_player_noise_0)
   .db TRM
 _sfx_expl_player_noise_0:
-  .db SLL,24,STV,5,SAR,2,STP,4,SDU,1,7
+  .db SLL,24,STV,5,SAR,2,STP,5,SDU,1,7
   .db RET
 
 _sfx_expl_blast:
@@ -464,6 +469,6 @@ _sfx_expl_org_noise:
   .db CAL,low(_sfx_expl_org_noise_0),high(_sfx_expl_org_noise_0)
   .db TRM
 _sfx_expl_org_noise_0:
-  .db SLL,24,STV,6,SAR,2,STP,4,SDU,0,6
+  .db SLL,24,STV,6,SAR,2,STP,5,SDU,0,6
   .db RET
 
