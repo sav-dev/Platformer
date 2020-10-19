2c3f076f169d33cc82741b67b4cb238a767c8690
sav-dev <tomasz.sawicki.dev@outlook.com>
2020-10-19 14:08:08 -0700

    3_8 update

--- platform.asm
+++ platform.asm
@@ -749,7 +749,7 @@ Bank06:
 Bank06Start:  
   
   level_3_01:
-  .incbin "data\levels\level_3_01.bin"
+  .incbin "data\levels\level_3_02.bin"
   level_3_08:
   .incbin "data\levels\level_3_08.bin"  
   
@@ -763,7 +763,7 @@ Bank07:
 Bank07Start:
 
   level_3_02:
-  .incbin "data\levels\level_3_02.bin"
+  .incbin "data\levels\level_3_01.bin"
   level_3_03:
   .incbin "data\levels\level_3_03.bin"
   level_3_04:
